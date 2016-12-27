cf26cf1ec9fedc7fbded8d348564e1399411f4ed
JonW27 <18wongj@gmail.com>
2016-12-26 22:55:24 -0500

    installed selenium webdriver, and have got it to bring up chrome browser, (gives an error though)

--- botto/Controller.java
+++ botto/Controller.java
@@ -1,11 +1,23 @@
 import java.io.*;
-import javax.mail.*;
+//import javax.mail.*;
 import java.util.*;
 import java.net.InetAddress;
-import com.sun.mail.smtp.*;
+//import com.sun.mail.smtp.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Controller{
   public static void main(String[] args){
-    System.out.println("Scaffolding worked!");
+    String lol = "testing/chromedriver";
+    System.setProperty("webdriver.chrome.driver", lol);
+    WebDriver driver = new ChromeDriver();
+    try{
+      driver.get("https://google.com");
+      System.out.println("Scaffolding worked! "+ driver.getTitle());
+      driver.quit();
+    }
+    catch(Throwable e){
+      e.printStackTrace();
+    }
   }
 }
