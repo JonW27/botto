a061f2a8485ac4c75b5dc97b757268350cddf71f
edmond120 <edmondwong888@msn.com>
2016-12-30 17:56:23 -0500

    made getUsername and getTimeStamp methods

--- botto/Controller.java
+++ botto/Controller.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 public class Controller{
-  public static WebElement getMessage(WebDriver driver){
+  public static WebElement getMessageGroup(WebDriver driver){
     List<WebElement> messages = driver.findElements(By.className("message-group"));
     int size = messages.size();
     return messages.get(size - 1);
@@ -23,9 +23,17 @@ public class Controller{
 	int size = getMarkups(message).size();
 	return getMarkups(message).get(size - 1).getText();
     }
-    public static void send(WebDriver driver,String str){
+    //getUsername is not a valid way of identification
+    public static String getUsername(WebElement messageGroup){
+	return messageGroup.findElement(By.className("user-name")).getText();
+    }
+    public static String getTimeStamp(WebElement messageGroup){
+	return messageGroup.findElement(By.className("timestamp")).getText();
+    }
+    public static boolean send(WebDriver driver,String str){
       driver.findElement(By.tagName("textarea")).sendKeys(str);
       driver.findElement(By.tagName("textarea")).sendKeys(Keys.RETURN);
+      return true;
     }
   public static void main(String[] args){
     String lol = "testing/chromedriver.exe";
@@ -46,16 +54,25 @@ public class Controller{
         driver.findElement(By.xpath("//*[contains(text(), 'Skip')]")).click();
       }
       driver.get("https://discordapp.com/channels/263162147792617482/263162147792617482");
-      System.out.println(getMessage(driver));
+      //System.out.println(getMessageGroup(driver));
+      String username = "bottodemo";
       while(true){
-	  WebElement message = getMessage(driver);
+	  WebElement message = getMessageGroup(driver);
 	  String markup = getMarkup(message);
-	  if(markup.equals("hi")){
-	      send(driver,"hello");
-	  }
-	  else if(markup.equals("break")){
-	      send(driver,"exiting loop");
-	      break;
+	  if(!(getUsername(message).equals(username))){
+	      if(markup.equals("hi")){
+		  send(driver,"hello " + getUsername(message));
+	      }
+	      else if(markup.equals("-time")){
+		  send(driver,getTimeStamp(message));
+	      }
+	      else if(markup.equals("-infinite loop")){
+		  send(driver,"hi");
+	      }
+	      else if(markup.equals("break")){
+		  send(driver,"exiting loop");
+		  break;
+	      }
 	  }
 	  TimeUnit.SECONDS.sleep(1);
       }
