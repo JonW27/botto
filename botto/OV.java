b572762dc395efb9a3ff49662ae292ce5e4eb1e4
JonW27 <18wongj@gmail.com>
2017-01-10 23:20:21 -0500

    testing java io editing support for addons

---
+++ botto/OV.java
@@ -0,0 +1,19 @@
+import org.apache.commons.io.FileUtils;
+import java.util.*;
+import java.io.File;
+public class OV{
+	public static void main(String args[]){
+		String string = "import org.openqa.selenium.WebDriver;\n";
+		try{
+			File file = new File("OV.java");
+			List fix = FileUtils.readLines(file, "UTF-8");
+			for(int i = 0; i < fix.size(); i++){
+				string = string + fix.get(i) + "\n";
+			}
+			FileUtils.writeStringToFile(file, string, "UTF-8");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
